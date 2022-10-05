ede434abc60b59327689c4ef45f1e169052b0b1d
remont212 <107880286+remont212@users.noreply.github.com>
2022-10-05 17:26:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+
+$(document).on("scroll", function(){
+    $("header").addClass("fixed")
+})
+
+$(document).on("scroll", function(){
+    $(".loader").addClass("fixed")
+})
+
+$("#click").on("click", () =>{
+    $(".loader0").addClass("loader");
+setTimeout(() => {
+    $(".form").css({
+        "display":"block"
+    })
+    $("#click").css({
+        "display":"none"
+    })
+    $("#click2").css({
+        "display":"block"
+
+    })
+}, 3000)
+});
+let counter = 3;
+$("#click2").on("click", () =>{
+    $(".loader0").addClass("loader");
+    let int = setInterval(() => {
+counter--;
+if(counter === 0){
+    clearInterval(int);
+alert("Вы закрыли форму")
+}
+}, 1000);
+
+setTimeout(() => {
+    $(".form").css({
+        "display":"none"
+    })
+    $("#click").css({
+        "display":"block"
+    })
+    $("#click2").css({
+        "display":"none"
+
+    })
+}, 3000)
+});
+
+$(".open").on("click", () => {
+    $(".modal").animate({
+        "top":"100"
+    }, 300)
+})
+
+$(".close").on("click", () => {
+    $(".modal").animate({
+        "top":"-700"
+    }, 300)
+})
+
